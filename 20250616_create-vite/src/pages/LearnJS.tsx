ac0c5d5b49c317dbdff7bf828a4d90a634fe6f6d
gear_kinu <techgit1024@gmail.com>
2025-06-25 23:57:03 +0900

    chore: async, await, try, catchの勉強

--- 20250616_create-vite/src/pages/LearnJS.tsx
+++ 20250616_create-vite/src/pages/LearnJS.tsx
@@ -2,8 +2,37 @@ import { useEffect } from "react";
 
 export const LearnJS = () => {
 
+    // async, await, try, catch, fetch
+    function handleClick() {
+        fetchData();
+        add();
+    }
+    async function fetchData() {
+        try {
+            const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+            const data = await response.json();
+            console.log(data);
+            console.log(data.id)
+        } catch (error) {
+            console.error("返事を受け取れませんでした：", error);
+        }
+    }
+
+    // try, catch
+    const add = async () => {
+        try {
+            const res = await fetch("https://api/")
+            const data = await res.json()
+        } catch (error) {
+        console.error("エラー：", error);
+        } finally {
+            console.log("finally")
+        }
+    }
+
     return (
         <>
+            <button onClick={handleClick}>ボタン</button>
         </>
     )
 }
